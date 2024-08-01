4f6f1f582c3f3f33ea204defa4f90cce1d4f5058
Shay <moeshakasanova@gmail.com>
2024-08-01 17:20:16 +0400

    created cart-dropdown component and relative styles

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,14 @@
+import "./navigation.styles.scss";
+import { useContext } from "react";
+
 import { Outlet, Link } from "react-router-dom";
 
 import { UserContext } from "../../contexts/user.context";
-
-import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { signOutUser } from "../../utils/firebase.utils";
 
-import "./navigation.styles.scss";
-import { useContext } from "react";
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
@@ -32,7 +34,9 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+        <CartDropdown />
       </div>
       <Outlet />
     </>
