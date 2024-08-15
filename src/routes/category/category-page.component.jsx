53d14eedd3bafd90087bfe11fa0edbdd4decee49
Shay <moeshakasanova@gmail.com>
2024-08-15 21:09:00 +0400

    go back button in category

--- src/routes/category/category-page.component.jsx
+++ src/routes/category/category-page.component.jsx
@@ -1,8 +1,8 @@
 import "./category.styles.scss";
 
 import { useContext, useState, useEffect, Fragment } from "react";
-import { useParams } from "react-router-dom";
-
+import { useParams, useNavigate } from "react-router-dom";
+import Button from "../../components/button/button.component";
 import ProductCard from "../../components/product-card/product-card.component";
 
 import { CategoriesContext } from "../../contexts/categories.context";
@@ -11,7 +11,11 @@ const Category = () => {
   const { category } = useParams();
   const { categoriesMap } = useContext(CategoriesContext);
   const [products, setProducts] = useState(categoriesMap[category]);
+  const navigate = useNavigate();
 
+  const goToShop = () => {
+    navigate("/shop");
+  };
   useEffect(() => {
     setProducts(categoriesMap[category]);
   }, [category, categoriesMap]);
@@ -19,6 +23,10 @@ const Category = () => {
   return (
     <Fragment>
       <h2 className="category-title">{category.toUpperCase()}</h2>
+      <Button buttonType="inverted" onClick={goToShop}>
+        Go Back
+      </Button>
+
       <div className="category-container">
         {products &&
           products.map((product) => (
