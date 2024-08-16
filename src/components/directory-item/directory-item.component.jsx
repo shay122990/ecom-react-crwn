59ddb71f64d98ae4431c9e40f1df7115b8900fb5
Shay <moeshakasanova@gmail.com>
2024-08-16 17:02:00 +0400

    goToCategory onClick directory item

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,7 +1,12 @@
 import "./directory-item.styles.scss";
+import { useNavigate } from "react-router-dom";
 
 const DirectoryItem = ({ category }) => {
   const { imageUrl, title } = category;
+  const navigate = useNavigate();
+  const goToCategory = () => {
+    navigate(`/shop/${title.toLowerCase()}`);
+  };
   return (
     <div className="directory-item-container">
       <div
@@ -10,7 +15,7 @@ const DirectoryItem = ({ category }) => {
           backgroundImage: `url(${imageUrl})`,
         }}
       />
-      <div className="body">
+      <div className="body" onClick={goToCategory}>
         <h2>{title}</h2>
         <p>Shop Now</p>
       </div>
