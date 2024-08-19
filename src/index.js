8ae79545db330f2ec0d8365a6e37aa36f88ed74e
Shay <moeshakasanova@gmail.com>
2024-08-19 18:09:28 +0400

    switching to redux, configuring store and boilerplate setup

--- src/index.js
+++ src/index.js
@@ -1,25 +1,30 @@
-import React from "react";
 import "./index.scss";
-import App from "./App";
+import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import { createRoot } from "react-dom/client";
+import App from "./App";
+
 import { UserProvider } from "./contexts/user.context";
 import { CategoriesProvider } from "./contexts/categories.context";
 import { CartProvider } from "./contexts/cart.context";
+import { store } from "./store/store";
 
 const container = document.getElementById("app");
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <UserProvider>
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        </UserProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
---
+++ src/store/store.js
@@ -0,0 +1,8 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import logger from "redux-logger";
+import { rootReducer } from "./root-reducer";
+
+const middleWares = [logger];
+const composedEnhancers = compose(applyMiddleware(...middleWares));
+
+export const store = createStore(rootReducer, undefined, composedEnhancers);
