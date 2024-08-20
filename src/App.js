acf98b41081733b90c517bd66c7ada38b168f619
Shay <moeshakasanova@gmail.com>
2024-08-20 15:31:12 +0400

    dispatch action from shop page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import "./index.scss";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { setCurrentUser } from "./store/user/user-action";
-import {
-  onAuthStateChangedListener,
-  createUserDocumentFromAuth,
-} from "./utils/firebase.utils";
+
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home-page";
@@ -14,20 +8,6 @@ import Shop from "./routes/shop/shop.component";
 import Checkout from "./routes/checkout/checkout-page";
 
 const App = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
-      if (user) {
-        console.log("User detected after redirect:", user);
-
-        createUserDocumentFromAuth(user);
-      } else {
-        console.log("No user detected.");
-      }
-      dispatch(setCurrentUser(user));
-    });
-    return unsubscribe;
-  }, []);
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
